8c9fa6bfbace81ab371987213062aebd3c6405ed
AnilAnjuna <anilanjuna@yahoo.com>
2014-02-26 07:39:09 +0530

    TNode.java

--- TNode.java
+++ TNode.java
@@ -20,13 +20,6 @@ public class TNode {
     
     
     
-//public TNode(int id,TNode left,TNode right){
-//        
-//        this.id = id;
-//        this.left = left;
-//        this.right = right;
-//        
-//    }
 
  public TNode(int id,String fname,String sname,String title,String bname,TNode left,TNode right){
         
@@ -39,5 +32,62 @@ public class TNode {
         this.right = right;
         
     }
+    
+     public int getId() {
+        return id;                          //generate getters and setters to easy implmentations
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TNode left) {
+        this.left = left;
+    }
+
+    public TNode getRight() {
+        return right;
+    }
+
+    public void setRight(TNode right) {
+        this.right = right;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+ 
     
 }
